eba5ea7e7caa575706e98e6545866dc1ad108509
Jason wang <2239429402wyh@gmail.com>
2025-05-29 12:39:30 +0800

    fix: 解决 Encoder_Decoder.py 的本地冲突，清理无用文件

---
+++ Layer/Norm.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+
+class LayerNorm(nn.Module):
+
+    "Construct a layernorm module (See citation for details)."
+    def __init__(self, features, eps=1e-6):
+        '''
+        :param features: 这个输入的是特征的维度数
+        :param eps: 1e-6
+        '''
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):    # [64,10,512]
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
