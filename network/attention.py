87f80bc4240f53556a8f692e058e91b33d37634e
Frank Wang <1183601004@cnu.edu.cn>
2025-05-30 15:40:02 +0800

    sb

--- network/attention.py
+++ network/attention.py
@@ -20,7 +20,7 @@ def attention(query, key, value, mask=None, dropout=None):
 
 
 class MultiHeadedAttention(nn.Module):
-    def __init__(self, h, d_model, dropout, local_context_length, device):
+    def __init__(self, h, d_model, local_context_length, device, dropout = 0.1):
         '''
         :param h: number of head
         :param d_model: Features
@@ -41,7 +41,7 @@ class MultiHeadedAttention(nn.Module):
         self.feature_weight_linear = nn.Linear(d_model, d_model)
         self.device = device
 
-    def forward(self, query, key, value, mask):
+    def forward(self, query, key, value, mask=None, dropout=0.1):
         if mask is not None:
             # Same mask applied to all h heads.
             mask = mask.unsqueeze(1)  # [batch_size, time_len, feature_dim]
@@ -56,6 +56,7 @@ class MultiHeadedAttention(nn.Module):
         ##################################query#################################################
 
         ########################################### local-attention ######################################################
+        '''
         local_weight_q = torch.matmul(query[:, self.local_context_length - 1:, :],# 未来时间N - 4，全部时间N
                                       query.transpose(-2, -1)) / ma.sqrt(q_size2)  # [batch_size, time_len-4, time_len]
         # [batch_size, time_len-4, time_len]->[batch_size, 1, 5* (time_len-4)]
@@ -64,29 +65,23 @@ class MultiHeadedAttention(nn.Module):
         local_weight_q_list = torch.cat(local_weight_q_list, 2)# 拼接softmax结果
         # [batch_size, 1, 5* (time_len-4)]->[batch_size, 5* (time_len-4), 1]
         local_weight_q_list = local_weight_q_list.permute(0, 2, 1)
-        # [batch_size, (time_len-4), feature]->[batch_size, 5* (time_len-4), 1]
-        q_list = [query[:, i: i + self.local_context_length, :] for i in range(q_size1)]
+        # [batch_size, (time_len-4), feature]->[batch_size, 5* (time_len-4), feature]
+        '''
+        q_list = [query[:, i: i + self.local_context_length, :] for i in range(q_size1-self.local_context_length+1)]
         q_list = torch.cat(q_list, 1)
-        # [128,11,5*31,1]*[128,11,5*31,2*12]->[128,11,5*31,2*12]
-        query = local_weight_q_list * q_list # 时间窗口*时间窗口
-        # [128,11,5*31,2*12]->[128,11,5,31,2*12] # contiguous() 会 重新分配内存，这样view可以直接根据处理好的query改
-        query = query.contiguous().view(q_size1, q_size0, self.local_context_length, q_size2, q_size3)
-        # [128,11,5,31,2*12]->[128,11,31,2*12]
-        query = torch.sum(query, 2)
-        # [128,11,31,2*12]->[128,2*12,11,31]
-        query = query.permute((0, 3, 1, 2))
+        # [batch_size, 5* (time_len-4), 1]*[batch_size, 5* (time_len-4), feature]->[batch_size, 5* (time_len-4), feature]
+        #query = local_weight_q_list * q_list # 时间窗口*时间窗口
+        query = q_list
+        # [batch_size, 5* (time_len-4), feature]->[batch_size, (time_len-4), 5, feature] # contiguous() 会 重新分配内存，这样view可以直接根据处理好的query改
+        query = query.contiguous().view(q_size0, q_size1-self.local_context_length+1, self.local_context_length, q_size2)
+        # [batch_size, (time_len-4), 5, feature]->[batch_size, (time_len-4), feature]
+        query = torch.sum(query, -2)
         ######################################################################################
-        query = query.permute((2, 0, 3, 1))  # [128,2*12,11,31] ->[11,128,31,2*12]
-        query = query.contiguous().view(q_size0 * q_size1, q_size2, q_size3)  # [11,128,31,2*12] ->[11*128,31,2*12]
-        query = query.contiguous().view(q_size0 * q_size1, q_size2, self.h, self.d_k).transpose(1,#分出来head，q_size3 = h*d_k
-                                                                                                2)  # [11*128,31,2*12] ->[11*128,31,2,12]->[11*109,2,31,12]
-        #####################################key#################################################
+        # [[batch_size, head, (time_len-4), feature/head]
+        query = query.contiguous().view(q_size0, q_size1, self.h, self.d_k).transpose(1,#分出来head，q_size3 = h*d_k
+                                                                                                2)
 
 
-        ##########################################context-aware key matrix############################################################################
-        # linar
-        key = self.conv_k(key)
-        key = key.permute((0, 2, 3, 1))  # [128,2*12,11,31+4]->[128,11,31+4,2*12]
         ########################################### local-attention ##########################################################################
         local_weight_k = torch.matmul(key[:, :, self.local_context_length - 1:, :], key.transpose(-2, -1)) / ma.sqrt(
             key_size3)  # [128,11,31,2*12] *[128,11,2*12,31+4]->[128,11,31,31+4]
@@ -104,24 +99,3 @@ class MultiHeadedAttention(nn.Module):
         # [128,11,5*31,2*12]->[128,11,5,31,2*12]
         key = key.contiguous().view(key_size1, key_size0, self.local_context_length, key_size2, key_size3)
         # [128,11,5,31,2*12]->[128,11,31,2*12]
-        key = torch.sum(key, 2)
-        # [128,11,31,2*12]->[128,2*12,11,31]
-        key = key.permute((0, 3, 1, 2))
-        #        key = self.conv_k(key)
-        key = key.permute((2, 0, 3, 1))
-        key = key.contiguous().view(key_size0 * key_size1, key_size2, key_size3)
-        key = key.contiguous().view(key_size0 * key_size1, key_size2, self.h, self.d_k).transpose(1, 2)
-        ##################################################### value matrix #############################################################################
-        value = value.view(key_size0 * key_size1, key_size2, key_size3)  # [4,128,31,2*12]->[4*128,31,2*12]
-        nbatches = q_size0 * q_size1
-        value = self.linears[0](value).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)  # [11*128,31,2,12]
-
-        ################################################ Multi-head attention ##########################################################################
-        x, self.attn = attention(query, key, value, mask=None,
-                                 dropout=self.dropout)
-        x = x.transpose(1, 2).contiguous() \
-            .view(nbatches, -1, self.h * self.d_k)
-        x = x.view(q_size0, q_size1, q_size2, q_size3)  # D[11,128,1,2*12] or E[11,128,31,2*12]
-
-
-        return self.linears[-1](x)
